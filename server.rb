9ae3fe9cc248e0fc7f3f203927e1cd2330df00ee
Ricky <ryim@dius.com.au>
2012-05-01 18:34:45 +1000

    more changes

--- server.rb
+++ server.rb
@@ -8,24 +8,14 @@ end
 
 post '/expense' do
   expense = JSON.parse(request.body.read)
-  
-  connection = Mongo::Connection.new
-  db = connection.db("mydb")
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  db = conn.db(uri.path.gsub(/^\//, ''))
   coll = db["expenses"]
   id = coll.insert(expense)
-  puts id
   id.to_s
 end
-# 
-# get '/expense' do
-#    connection = Mongo::Connection.new
-#    db = connection.db("mydb")
-#    coll = db["expenses"]
-#    expenses = coll.find.collect {|expense| expense.to_json}
-#    
-#    expenses.join
-# end
-# 
+
 get '/expense' do
   uri = URI.parse(ENV['MONGOHQ_URL'])
   conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
